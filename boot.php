13c510cdbc68fd1a76bd0511d1dc683e7405e621
miguelramos <miguel.marques.ramos@gmail.com>
2012-12-03 18:24:39 +0000

    Refactor repo.
    Correcção de erros no composer.json.

---
+++ boot.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * ------------------------------------------------------------------------------------
+ * boot.php
+ * ------------------------------------------------------------------------------------
+ *
+ * @package Websublime
+ * @author 	Miguel Ramos <miguel.marques.ramosgmail.com>
+ * @link 	https://www.websublime.com
+ * @version 0.3
+ */
+
+/* @end boot.php */
